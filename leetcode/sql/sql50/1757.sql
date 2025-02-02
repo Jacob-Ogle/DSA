22dc07c40f462d32da6b42572731d969367c3f40
Jacob <jacob-ogle@outlook.com>
2025-02-01 21:05:20 -0500

    Update 1757.sql

--- leetcode/sql/sql50/1757.sql
+++ leetcode/sql/sql50/1757.sql
@@ -1,2 +1,6 @@
+-- Problem: 1757. Recyclable and Low Fat Products
+-- https://leetcode.com/problems/recyclable-and-low-fat-products/description/?envType=study-plan-v2&envId=top-sql-50
+-- Easy
+
 select product_id from Products
 where low_fats = 'Y' and recyclable = 'Y'
