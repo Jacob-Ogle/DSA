3e2828c16d66fd53706ff8cc4b2dd049ffe5b50f
Jacob Ogle <jacob-ogle@outlook.com>
2025-02-06 14:15:31 -0500

    max depth binary tree w/ varying methods

--- leetcode/general/top_interview_150/binary_tree/104_max_depth.py
+++ leetcode/general/top_interview_150/binary_tree/104_max_depth.py
@@ -1,3 +1,4 @@
+from collections import deque
 from typing import Optional
 
 # Definition for a binary tree node.
@@ -13,3 +14,37 @@ class Solution:
             return 0
         
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+    
+    def maxDepthIterBFS(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        
+        level = 0
+        q = deque([root])
+        
+        while q:
+            
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+            level += 1
+        
+        return level
+    
+    def maxDepthIterDFS(self, root: Optional[TreeNode]) -> int:
+        stack = [[root, 1]]
+        result = 0
+
+        while stack:
+            node, depth = stack.pop()
+            
+            if node:
+                result = max(depth, result)
+                stack.append([node.left, depth+1])
+                stack.append([node.right, depth+1])
+
+        return result
